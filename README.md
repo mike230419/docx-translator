aefb28f9c65ea2ef16d358092a06eec2236fe90b
Mike Waldenmaier <mikewaldenmaier@yahoo.com>
2025-04-18 10:05:27 +0200

    feat: make repo ready for public

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
 # docx-translator
 
-Python script to translate a docx into a specified language
+Python script to translate a docx into a specified language using the DEEPL Document Translation API.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
 
 ## How to use:
 
-1. Create a file called `Wochenplan.docx` in the same directory as the EXE
-1. Run the EXE
+1. Place the `.exe` and the `.env` file in the same directory as your docx file.
+1. Open the `.env` file and enter your set your environment values (see [Environment Variables](#environment-variables)).
+1. Run the EXE and wait for the translation to finish. (This might take a while)
 1. Enjoy your translated document
 
-## Important
+## Environment Variables
 
-Before committing to main or merging your PR, make sure to update the tag number in the workflow file.
+| KEY             | VALUE                                                                                                                             |
+| --------------- | --------------------------------------------------------------------------------------------------------------------------------- |
+| SOURCE_LANGUAGE | The language of the document you want to translate. For example, if you want to translate from English to German, set this to EN. |
+| TARGET_LANGUAGE | The language you want to translate to. For example, if you want to translate from English to German, set this to DE.              |
+| DEEP_L_API_KEY  | Your DEEPL API key. You can get a free API key from the DEEPL website.                                                            |
+| API_URL         | The url to the DEEPL API, e.g. https://api-free.deepl.com/v2/document if you are using the free API.                              |
+| FILE_NAME       | The name of the file you want to translate.                                                                                       |
+
+## Contributing
+
+If you want to contribute to this project, feel free to open an issue or a pull request. I will try to respond as soon as possible.
---
+++ translate.py
@@ -0,0 +1,67 @@
+"""
+translate.py
+This script translates a DOCX file using the DeepL API.
+It reads the file, sends it to the DeepL API for translation, and saves the translated file.
+"""
+import time
+import requests
+
+
+def translate_file(api_key, api_url, target_lang, source_lang, file_name):
+    """
+    Translates a DOCX file using the DeepL API.
+    Args:
+        api_key (str): Your DeepL API key.
+        api_url (str): The DeepL API URL.
+        target_lang (str): The target language for translation.
+        source_lang (str): The source language of the document.
+        file_name (str): The name of the DOCX file to be translated.
+    """
+    with open(file_name, "rb") as file:
+        docx_binary = file.read()
+        headers = {
+            "Authorization": f"DeepL-Auth-Key {api_key}",
+        }
+        body = {
+            "source_lang": source_lang,
+            "target_lang": target_lang,
+        }
+        files = {
+            "file": (file_name, docx_binary),
+        }
+    response = requests.post(api_url, headers=headers,
+                             data=body, files=files, timeout=60)
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}")
+        print(response.text)
+        return
+    document_id = response.json().get("document_id")
+    document_key = response.json().get("document_key")
+
+    # Wait for the translation to finish
+    url = f"{api_url}/{document_id}"
+    body = {
+        "document_key": document_key,
+    }
+    response = requests.post(url, headers=headers, data=body, timeout=60)
+    while response.json().get("status") == "translating":
+        print("Waiting for translation to finish...")
+        time.sleep(response.json().get("seconds_remaining", 5))
+        response = requests.post(url, headers=headers, data=body, timeout=60)
+
+    if response.json().get("status") != "done":
+        print("Error: Translation failed")
+        print(response.text)
+        return
+
+    # Download the translated file
+    response = requests.post(
+        f"{api_url}/{document_id}/result", headers=headers, data=body, timeout=60)
+
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}")
+        print(response.text)
+        return
+    with open("translated_file.docx", "wb") as translated_file:
+        translated_file.write(response.content)
+    print("Translation completed successfully. Translated file saved as 'translated_file.docx'.")
